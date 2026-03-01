eec53566827e73e6bcfc7181e8b180be26ad3f9f
unknown <jorgemantinanmontero@gmail.com>
2026-03-01 23:57:52 +0100

    Fix Try to run the buttons 1.3, added <meta> to github pages run the code javascript

---
+++ app/+html.tsx
@@ -0,0 +1,23 @@
+import { ScrollViewStyleReset } from 'expo-router/html';
+import { type PropsWithChildren } from 'react';
+
+export default function Root({ children }: PropsWithChildren) {
+  return (
+    <html lang="es">
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+        {/* Esta línea permite que tus botones de JavaScript funcionen en GitHub Pages */}
+        <meta 
+          httpEquiv="Content-Security-Policy" 
+          content="script-src 'self' 'unsafe-inline' 'unsafe-eval' https://manti-twitch-backend.onrender.com;" 
+        />
+
+        <ScrollViewStyleReset />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
