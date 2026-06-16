286262c312d3ce4c24ec78e48a54d41b1bab2866
unknown <jorgemantinanmontero@gmail.com>
2026-06-16 13:58:43 +0200

    fix: Cypress baseUrl should be root, not /manti-twitch (dev server serves at root)

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:8081/manti-twitch',
+    baseUrl: 'http://localhost:8081',
     supportFile: false,
   },
 })
